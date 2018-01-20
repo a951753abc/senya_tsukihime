7422b634c744871d08ffa3ef7ccb5675a37b8a97
Shion <a951753abc@gmail.com>
2018-01-20 18:49:53 +0800

    css fix

--- butouka.php
+++ butouka.php
@@ -1,5 +1,5 @@
 ﻿<!DOCTYPE html>
-
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
@@ -63,7 +63,7 @@
     <div id="description">
         <h1><a id="description_rank3"></a>取得特技 <a href="#top" style="text-decoration:none;color:#000;">▲</a></h1>
         <h2>初期取得：<br>得到「武道」，然後再選擇一個自己喜歡的特技。</h2>
-        <h2>升級：<br>等級上升之時、選取一個喜歡的特技。每逢５等、１０等時，可以額外再取得一個特技。</h2>
+        <h2>升級：<br>等級上升之時、選取一個喜歡的特技。5等、10等時，可以額外再取得一個特技。</h2>
     </div>
 
     <!-- 分隔線 -->
@@ -580,45 +580,11 @@
 
 
                 <!-- 分類 -->
-                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">常時</div>
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">通用</div>
                 <br/>
 
                 <!-- 代價 -->
-                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">－</div>
-                <br/>
-
-                <!-- 情緒獎勵 -->
-                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">０</div>
-                <br/>
-
-                <!-- 取得限制 -->
-                <div class="skillTh2">取得限制</div><div class="skillTd3">－</div>
-                <br/>
-
-                <!-- 特技解說 -->
-
-                <!-- 效果 -->
-                <p class="skillFunction"><b>効果：</b>實際傷害後效果，對手的情緒值減去同等傷害骰的數值。</p>
-
-                <!-- 結尾 -->
-            </div>
-        </skillbox>
-
-        <skillbox>
-            <div class="skillWrap">
-
-                <!-- 特技名稱 -->
-                <h3 class="skillTitle">真氣統合</h3>
-
-                <!-- 特技設定 -->
-
-
-                <!-- 分類 -->
-                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">準備階段</div>
-                <br/>
-
-                <!-- 代價 -->
-                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">－</div>
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">代價傷害4</div>
                 <br/>
 
                 <!-- 情緒獎勵 -->
@@ -633,9 +599,41 @@
 
                 <!-- 效果 -->
                 <p class="skillFunction"><b>効果：</b>
-                    你將現有HP與TP加總後均分。 <BR>
-                    均分後的HP每高過最大HP5點，獲得1點【真氣】。 <BR>
-                    每1點真氣提升你1D6的傷害，並視為額外的生命值，當你受到傷害時，優先扣除真氣。
+                    實際傷害後，對方情緒值減去《3D6》。每當傷害骰出現一個6或5，額外增加1D6+1。
+                </p>
+
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">真氣統合</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">準備階段</div>
+                <br/>
+
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">－</div>
+                <br/>
+
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">獲得真氣時上升2</div>
+                <br/>
+
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">－</div>
+                <br/>
+
+                <!-- 特技解說 -->
+
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    你的武鬥家等級必須超過其他級別。 <BR>
+                    你將現有HP與TP加總後均分。均分後若回復超過原先HP5點，獲得狀態【真氣】。 <BR>
+                    【真氣】：傷害增加【武鬥家等級】，此回合戰鬥判定增加1D6。狀態持續到你遭受傷害為止。
                 </p>
 
                 <!-- 結尾 -->
@@ -877,11 +875,11 @@
                 <br/>
 
                 <!-- 代價 -->
-                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">1點真氣</div>
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">-</div>
                 <br/>
 
                 <!-- 情緒獎勵 -->
-                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">０</div>
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
                 <br/>
 
                 <!-- 取得限制 -->
@@ -892,8 +890,9 @@
 
                 <!-- 效果 -->
                 <p class="skillFunction"><b>効果：</b>
-                    命中後效果，選擇本次攻擊中尚未使用的攻擊類型／近戰技能，視同直接命中。 <BR>
-                    並將傷害分段計算後加總。
+                    真氣狀態下使用。 <BR>
+                    命中後效果，選擇本次攻擊中尚未使用的攻擊類型／近戰技能，視同直接命中，傷害分段計算後加總。 <BR>
+                    使用後真氣狀態解除。
                 </p>
 
                 <!-- 結尾 -->
@@ -915,7 +914,7 @@
                 <br/>
 
                 <!-- 代價 -->
-                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">0</div>
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">-</div>
                 <br/>
 
                 <!-- 情緒獎勵 -->
@@ -931,9 +930,9 @@
                 <!-- 效果 -->
                 <p class="skillFunction"><b>効果：</b>
                     戰鬥中使用。 <BR>
-                    回合中輪到你行動時，可將剩餘行動點全數轉為TP <BR>
-                    回合結束前你的被動判定減少1D6，受到攻擊時傷害額外增加2D6。 <BR>
-                    結束階段你獲得1真氣。
+                    回合中輪到你行動時，可將剩餘行動點全數轉為TP。 <BR>
+                    回合結束前你的被動判定減少1D6，受到攻擊時傷害額外增加2D6-【武鬥家等級】。 <BR>
+                    結束階段你獲得〈真氣〉。
                 </p>
 
                 <!-- 結尾 -->
@@ -955,7 +954,7 @@
                 <br/>
 
                 <!-- 代價 -->
-                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">1點真氣</div>
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">-</div>
                 <br/>
 
                 <!-- 情緒獎勵 -->
@@ -970,15 +969,309 @@
 
                 <!-- 效果 -->
                 <p class="skillFunction"><b>効果：</b>
-                    受到近戰攻擊時可使用，本次的迴避判定增加1D6。 <BR>
-                    迴避成功時，選擇一攻擊類型／近戰攻擊對手，並無須擲骰直接命中。
+                    真氣狀態下使用。 <BR>
+                    受到近戰攻擊時，迴避判定+1D6。 <BR>
+                    迴避成功時，選擇一攻擊類型／近戰攻擊對手，無須擲骰直接命中，對方無法使用技能類型:防禦。 <BR>
+                    使用後真氣解除。
                 </p>
-
                 <!-- 結尾 -->
-
             </div>
         </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">真氣制御‧浮生</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">通用</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">-</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
+                <br/>
 
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">真氣統合、武鬥家等級4以上</div>
+                <br/>
+
+                <!-- 特技解說 -->
+
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    真氣狀態下使用，一劇本2次。 <BR>
+                    當你受到攻擊時，可將一半所受到的傷害由TP支付，並且保持〈真氣〉。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">超凡</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">通用</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">-</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">〈真氣〉系列技能4個或武神的呼吸</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    一劇本1次，若達成所有取得限制，使用次數+1。 <BR>
+                    下列效果三擇一： <BR>
+                    ●無盡破壞或是一個特技的代價變成0。 <BR>
+                    ●限制次數的能力回數+1。 <BR>
+                    ●此回合判定不會遭受其他人的技能或背反律所影響。 <BR>
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">自然之體</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">常時</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">-</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">創角時限定獲得、HP上限-10</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    你的所有屬性點+3，回合結束階段回復1D6TP。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">氣入</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">攻擊類型／近戰</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">代價傷害2</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">無</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    指定對象一人，給予其《1D6+[你的一屬性值]》傷害。若該屬性值高於5點，實傷階段額外造成[你的一屬性值]。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">縫之牢</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">通用</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">代價傷害7</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">無</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    實傷後效果，對方下次逃跑判定達成值-《2》，命中後對象獲得不良狀態【鎖定】。 <BR>
+                    【鎖定】：當你受到來自鎖定者的攻擊時，鎖定者的命中判定+3，會心-1。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">伏龍滅</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">進攻／近戰</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">代價傷害6</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">2</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">無</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    攻擊範圍增加至鄰近區域，傷害增加《3》。搭配〈氣入〉合算為單一技能，且代價傷害改為4。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">三絕掌</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">通用</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">代價傷害30</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">2</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">武鬥家任意三個攻擊類型／近戰、武鬥家等級6以上</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    攻擊類型／近戰技能命中後，可對同一目標選擇一個你本回尚未使用過的攻擊類型／近戰技能進行攻擊，若命中還能再次選擇一個本回合尚未使用過的攻擊類型／近戰技能進行一次攻擊。 <BR>
+                    實際傷害分開計算。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">悟天霞</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">常時</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">-</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">自然之體、武鬥家等級3以上</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    HP和TP增加（武鬥家等級*2），精神判定常時+3。失去所有的防禦點，換成等量的屬性技能點數，並可隨意分配到任意屬性上。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap_dividepoint">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">八極拳</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:600px;">攻擊類型／近戰</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:600px;">代價傷害8</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">無</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    指定對象一人，造成1D6+【雙方防禦點差】的傷害。 <BR>
+                    武鬥家等級四時，此技能效果改為： <BR>
+                    指定對象一人，造成2D6+【對手防禦點】的傷害。 <BR>
+                    <BR>
+                    武鬥家等級六時，此技能效果改為： <BR>
+                    指定對象一人，造成3D6+【雙方防禦總合】的傷害。 <BR>
+                    <BR>
+                    武鬥家等級十時，此技能效果改為： <BR>
+                    指定對象一人，造成4D6+【雙方防禦總合】的傷害，此技能實際傷害為兩倍。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">七脈淨氣</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">進攻／近戰</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">代價傷害30</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">武鬥家等級8以上</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    此次傷害增加《4》D6+2的傷害，命中增加[你的一屬性值]。 <BR>
+                    額外支付10點代價傷害，本次攻擊增加攻擊範圍內一目標。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
+        <skillbox>
+            <div class="skillWrap">
+                <!-- 特技名稱 -->
+                <h3 class="skillTitle">武練</h3>
+                <!-- 特技設定 -->
+                <!-- 分類 -->
+                <div class="skillTh clear">分類</div><div class="skillTd" style="width:265px;">通用</div>
+                <br/>
+                <!-- 代價 -->
+                <div class="skillTh clear">代價</div><div class="skillTd" style="width:265px;">代價傷害5</div>
+                <br/>
+                <!-- 情緒獎勵 -->
+                <div class="skillTh2 clear">情緒獎勵</div><div class="skillTd3">0</div>
+                <br/>
+                <!-- 取得限制 -->
+                <div class="skillTh2">取得限制</div><div class="skillTd3">初始級別皆為武鬥家</div>
+                <br/>
+                <!-- 特技解說 -->
+                <!-- 效果 -->
+                <p class="skillFunction"><b>効果：</b>
+                    一場戰鬥兩次。 <BR>
+                    命中或迴避時可骰兩次判定，擇較高數為達成值，但無法會心。
+                </p>
+                <!-- 結尾 -->
+            </div>
+        </skillbox>
         <div id="clearleft"></div>
 
     </div>
